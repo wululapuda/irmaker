2b96062f6a9c9c53e4c8b89f6f39b4473593ce8b
wululapuda <160254267+wululapuda@users.noreply.github.com>
2025-08-11 21:07:06 +0800

    Add files via upload

---
+++ libs/lang.py
@@ -0,0 +1,13 @@
+import json
+class lang:
+    def __init__(self):#实例化类就加载
+
+    data = None#语言字典存储变量
+class function(lang):
+    def change(self, langkind):
+        try:
+            with open("lang" + "\\" + langkind + ".json", "r") as lang_files:
+                jsondata = lang_files.readline()
+                self.data = json.load(jsondata)
+                lang_files.close()
+
