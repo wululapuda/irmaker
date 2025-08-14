7972da22e918d7e35861d2b38d7eebbec30f79f9
wululapuda <15633726895@163.com>
2025-08-14 11:59:59 +0800

    新增错误

--- libs/error.py
+++ libs/error.py
@@ -5,3 +5,10 @@ class LangError(Exception):
     def __str__(self):
         return f"LangError + {self.massage}"
         return f"语言文件出现错误,错误代码 + {self.error_code}"
+class ProjectFileError(Exception):
+    def __init__(self,massage,error_code):
+        self.massage = massage
+        self.error_code = error_code
+    def __str__(self):
+        return f"ProjectFileError + {self.massage}"
+        return f"code + {self.error_code}"
