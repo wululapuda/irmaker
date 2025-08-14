6ace05ca890842ce25e05d1c87083488aed5ba17
wululapuda <15633726895@163.com>
2025-08-14 11:03:10 +0800

    新增函数

--- irmaker.py
+++ irmaker.py
@@ -8,3 +8,16 @@ class config_read:
     config.read("libs\\config\\CONFIG.ini")
     def lang(self):
         return self.config["lang"]["lang_type"]
+
+def create_project(name:str,version,description:str,path):
+    number = 0
+    if version == "1.12.2":
+        number = 3
+    if version == "1.16.5":
+        number = 6
+    with open(path+name+".irmaker","rb+") as f:
+        f.writelines(["packname"+"  "+name,"gameversion"+"  "+version+"\n","{\n\n"])
+        f.close()
+    with open(path+"pack.mcmeta","w") as f:
+        f.writelines(["{\n","  \"pack\":{\n","    \"pack_format\": "+str(number)+"\n","    \"description\": "+"\""+description+"\""+"\n","  }\n","}"])
+        f.close()
