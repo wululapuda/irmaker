8f1c1733fe509639b8d15d69257829d0e8ce6ee1
wululapuada <15633726895@163.com>
2026-02-03 16:15:54 +0800

    新增颜色切换功能

--- irmaker.py
+++ irmaker.py
@@ -9,6 +9,7 @@ import json
 import logging
 import datetime
 
+
 theme = {"1": ["#4A6572", "#E1E8ED"],
          "2": ["#5B7876", "#EDF3F2"],
          "3": ["#A1887F", "#F5EFEB"],
@@ -29,12 +30,16 @@ def config_get(key1, key2):
     config.read("libs/config.ini")
     return config.get(key1, key2)
 def confgi_set(key1,key2,value):
-    cofig = configparser.ConfigParser()
-    cofig.read("libs/config.ini")
-    cofig.set(key1,key2,value)
+    config = configparser.ConfigParser()
+    config.read("libs/config.ini")
+    config.set(key1,key2,value)
+    with open("libs/config.ini","w") as f:
+        config.write(f)
 
 class DateWrite:
-
+    def __init__(self,date:dict,file:str):
+        with open(file,"w",encoding="utf-8") as f:
+            json.dumps(date,f,ensure_ascii=False, indent=3)
 
 
 class Langauge:
@@ -48,27 +53,44 @@ class Langauge:
 
 
 class gui:
+    Showing_gui = []
+    def Update(self,*args):
+        for i in self.Showing_gui:
+            i.updateGUI()
     class Common:
         def run(self):
             self.screen.mainloop()
 
         def CutDown(self):
             self.screen.destroy()
+        def register(self):
+            gui.Showing_gui.append(self)
+            print(gui.Showing_gui)
+        def unregister(self):
+            self.Code = gui.Showing_gui.index(self)
+            gui.Showing_gui[self.Code].screen.destroy()
+            del gui.Showing_gui[self.Code]
+            print(gui.Showing_gui)
+        def updateGUI(self):
+            for i in self.screen.winfo_children():
+                i.destroy()
+                print(gui.Showing_gui)
+            self.pack()
 
-        def UpdateColor(self):
-            pass
-
-        def UpdateLanguage(self):
-            logging.info("Change Language")
 
     class MainGUI(Common):
         def __init__(self):
-            self.theme = config_get("User", "theme")
+            self.register()
             self.screen = tkinter.Tk()
             self.screen.title("IR Maker")
             self.screen.iconbitmap(f"libs/image/logo.ico")
             self.screen.geometry("600x350+450+200")
             self.screen.resizable(False, False)
+            self.screen.protocol("WM_DELETE_WINDOW",self.unregister)
+            self.pack()
+
+        def pack(self):
+            self.theme = config_get("User", "theme")
             self.frame1 = tkinter.Frame(master=self.screen, bd=165, width=450, bg="#E1E8ED")
             self.frame1.place(x=150, y=0, width=450, height=350)
             self.label1 = tkinter.Label(master=self.frame1,
@@ -112,15 +134,20 @@ class gui:
 
     class CreateWorkGUI(Common):
         def __init__(self):
-            self.work_type = tkinter.IntVar(value=1)
-            self.work_type.set(1)
+            self.work_type = tkinter.StringVar(value="immersiveRailroading")
+            self.work_type.set("immersiveRailroading")
             self.theme = config_get("User", "theme")
-            self.screen = tkinter.Tk(screenName="creatework")
+            self.register()
+            self.screen = tkinter.Toplevel()
+            self.screen.wm_attributes("-topmost")
             self.screen.title("IR Maker")
-            self.screen.config(bg=theme[self.theme][0])
             self.screen.iconbitmap(f"libs/image/logo.ico")
-            self.screen.geometry("450x500+550+200")
+            self.screen.geometry("450x500")
             self.screen.resizable(False, False)
+            self.screen.protocol("WM_DELETE_WINDOW", lambda: self.unregister())
+            self.pack()
+        def pack(self):
+            self.screen.config(bg=theme[self.theme][0])
             self.frame1 = tkinter.Frame(master=self.screen, bg=theme[self.theme][0])
             self.frame1.place(x=0, y=0, width=100, height=500)
             self.frame2 = tkinter.Frame(master=self.screen, bg=theme[self.theme][1])
@@ -134,8 +161,7 @@ class gui:
                                                activebackground=theme[self.theme][1],
                                                activeforeground=theme[self.theme][0],
                                                variable=self.work_type,
-                                               value=1
-
+                                               value="immersiveRailroading"
                                                )
             self.button1.pack(anchor="nw", fill="both")
             self.button2 = tkinter.Radiobutton(master=self.frame1,
@@ -146,22 +172,35 @@ class gui:
                                                activebackground=theme[self.theme][1],
                                                activeforeground=theme[self.theme][0],
                                                variable=self.work_type,
-                                               value=2
+                                               value="RealTrainMod",
                                                )
             self.button2.pack(anchor="nw", fill="both")
-            self.button1.bind("<Button-1>", self.update)
-
-        def update(self, event):
-            self.frame2.destroy()
-            self.frame2 = tkinter.Frame(master=self.screen, bg=theme[self.theme][1])
-            self.frame2.place(x=100, y=0, width=350, height=500)
-
-            if self.work_type.get() == 1:
+            self.work_type.trace("w", self.update)
+            self.update(event=0)
+        def update(self,event,*args):
+            for i in self.frame2.winfo_children():
+                i.destroy()
+            if self.work_type.get() == "immersiveRailroading":
                 self.ImmersiveRailroading()
             else:
                 pass
 
         def ImmersiveRailroading(self):
+            """self.frame[num] = tkinter.Frame(master=self.frame2, bg=theme[self.theme][1])
+            self.frame[num].pack(anchor="n", fill="both", side="top")
+            tkinter.Label(master=self.frame[num], bg=theme[self.theme][1]).pack(anchor="n", fill="x", side="top")
+            tkinter.Label(master=self.frame[num], bg=theme[self.theme][1]).pack(anchor="n", side="left", fill="none")
+            self.label[num] = tkinter.Label(master=self.frame6, bg=theme[self.theme][1],
+                                        text=Langauge().language['lang']['label']["create_project_version"] + ":")
+            self.label[num].pack(anchor="n", fill="x", side="left")
+            something.....
+            """
+            def ask_file(key):
+                path = tkinter.filedialog.askdirectory()
+                key.delete(0,1000000)
+                key.insert(0,path)
+            def creatework():
+                pass
             self.frame3 = tkinter.Frame(master=self.frame2, bg=theme[self.theme][1])
             self.frame3.pack(anchor="n", fill="both", side="top")
             tkinter.Label(master=self.frame3, bg=theme[self.theme][1]).pack(anchor="n", fill="x", side="top")
@@ -172,6 +211,7 @@ class gui:
             self.label1.pack(anchor="w", fill="none", side="left")
             self.text1 = tkinter.Entry(master=self.frame3, width=35)
             self.text1.pack(anchor="n", fill="x", side="left")
+
             self.frame4 = tkinter.Frame(master=self.frame2, bg=theme[self.theme][1])
             self.frame4.pack(anchor="n", fill="both", side="top")
             tkinter.Label(master=self.frame4, bg=theme[self.theme][1]).pack(anchor="n", fill="x", side="top")
@@ -181,6 +221,7 @@ class gui:
             self.label2.pack(anchor="n", fill="x", side="left")
             self.text2 = tkinter.Entry(master=self.frame4, width=35)
             self.text2.pack(anchor="n", fill="x", side="left")
+
             self.frame5 = tkinter.Frame(master=self.frame2, bg=theme[self.theme][1])
             self.frame5.pack(anchor="n", fill="both", side="top")
             tkinter.Label(master=self.frame5, bg=theme[self.theme][1]).pack(anchor="n", fill="x", side="top")
@@ -190,6 +231,7 @@ class gui:
             self.label3.pack(anchor="n", fill="x", side="left")
             self.choose1 = ttk.Combobox(master=self.frame5, width=32, values=["1.12.2", "1.16.5"])
             self.choose1.pack(anchor="n", fill="none", side="left")
+
             self.frame6 = tkinter.Frame(master=self.frame2, bg=theme[self.theme][1])
             self.frame6.pack(anchor="n", fill="both", side="top")
             tkinter.Label(master=self.frame6, bg=theme[self.theme][1]).pack(anchor="n", fill="x", side="top")
@@ -203,8 +245,21 @@ class gui:
                                           text=Langauge().language['lang']['button']["create_work_Create"])
             self.button3.pack(anchor="se", fill="none", padx=25, pady=26, side="bottom")
 
+            self.frame7 = tkinter.Frame(master=self.frame2, bg=theme[self.theme][1])
+            self.frame7.pack(anchor="n", fill="both", side="top")
+            tkinter.Label(master=self.frame7, bg=theme[self.theme][1]).pack(anchor="n", fill="x", side="top")
+            tkinter.Label(master=self.frame7, bg=theme[self.theme][1]).pack(anchor="n", side="left", fill="none")
+            self.label5 = tkinter.Label(master=self.frame7, bg=theme[self.theme][1],
+                                        text=Langauge().language['lang']['label']["create_project_filepath"] + ":   ")
+            self.label5.pack(anchor="n", fill="x", side="left")
+            self.text4 = tkinter.Entry(master=self.frame7,width=31)
+            self.text4.pack(anchor="n", fill="none", side="left")
+            self.button4 = ttk.Button(master=self.frame7,width=2,text="...",command=lambda:ask_file(self.text4))
+            self.button4.pack(anchor="e", fill="none", side="left")
+
     class SettingGUI(Common):
         def __init__(self):
+            self.register()
             self.value_color = tkinter.Variable()
             self.value_color.set(value=config_get("User", "theme"))
             self.theme = config_get("User", "theme")
@@ -213,6 +268,9 @@ class gui:
             self.screen.iconbitmap(f"libs/image/logo.ico")
             self.screen.geometry("600x350+550+180")
             self.screen.resizable(False, False)
+            self.screen.protocol("WM_DELETE_WINDOW", lambda: self.unregister())
+            self.pack()
+        def pack(self):
             self.frame1 = tkinter.Frame(master=self.screen, bd=165, width=450, bg="#E1E8ED")
             self.frame1.place(x=150, y=0, width=450, height=350)
             self.label1 = tkinter.Label(master=self.frame1,
@@ -273,7 +331,10 @@ class gui:
             else:
                 self.screen.config(bg=config_get("User", "CustomColor_deep"))
                 self.frame1.config(bg=config_get("User", "CustomColor_light"))
-
+            self.value_color.trace("w", lambda *args:self.changecolor())
+        def changecolor(self):
+            confgi_set("User","theme",str(self.value_color.get()))
+            gui().Update()
 
 def init():
     gui.MainGUI().run()
